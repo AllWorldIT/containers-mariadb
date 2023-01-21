01f58ab255ac1a3c056caf2f632f1f9995373496
Nigel Kukard <nkukard@lbsd.net>
2023-01-21 07:23:18 +0000

    feat: migrated to new FDC logging framework

--- usr/local/share/flexible-docker-containers/healthcheck.d/42-mariadb.sh
+++ usr/local/share/flexible-docker-containers/healthcheck.d/42-mariadb.sh
@@ -21,9 +21,9 @@
 
 
 if ! MARIADB_TEST_RESULT=$(mariadb-admin ping 2>&1); then
-    echo -e "ERROR: Healthcheck failed for MariaDB:\n$MARIADB_TEST_RESULT"
+    fdc_error "Health check failed for MariaDB:\n$MARIADB_TEST_RESULT"
     false
 fi
-if [ -n "$CI" ]; then
-    echo -e "INFO: Healthcheck for MariaDB:\n$MARIADB_TEST_RESULT"
+if [ -n "$FDC_CI" ]; then
+    fdc_info "Health check for MariaDB:\n$MARIADB_TEST_RESULT"
 fi
--- usr/local/share/flexible-docker-containers/tests.d/43-mariadb-cluster.sh
+++ usr/local/share/flexible-docker-containers/tests.d/43-mariadb-cluster.sh
@@ -29,15 +29,15 @@ function wait_for_startup() {
 
 	for i in {240..0}; do
 		got_value=$(echo "SELECT $column FROM $table" | mariadb -s "$database" 2>&1) || true
-        echo "TEST PROGRESS (mariadb): Got\n$got_value"
+        fdc_test_progress mariadb "Got\n$got_value"
 		if [ "$got_value" = "$check" ]; then
 			break
 		fi
-		echo "TEST PROGRESS (mariadb): Waiting for correct value ('$database', '$table', '$column')... ${i}s"
+		fdc_test_progress mariadb "Waiting for correct value ('$database', '$table', '$column')... ${i}s"
 		sleep 1
 	done
 	if [ "$i" = 0 ]; then
-		echo -e "TEST FAILED (mariadb): Database did not return correct value ('$database', '$table', '$column')\nResult: $got_value"
+		fdc_test_failed mariadb "Database did not return correct value ('$database', '$table', '$column')\nResult: $got_value"
 		return 1
 	fi
     return
@@ -45,64 +45,58 @@ function wait_for_startup() {
 
 
 # We only run this test for cluster nodes
-[ "$FDC_CI" != "cluster-node1" -a "$FDC_CI" != "cluster-node2" -a "$FDC_CI" != "cluster-node3" ] && return
+if [ "$FDC_CI" != "cluster-node1" ] && [ "$FDC_CI" != "cluster-node2" ] && [ "$FDC_CI" != "cluster-node3" ]; then
+    return
+fi
 
-echo "TEST START (mariadb): Testing default setup"
+fdc_test_start mariadb "Testing database table value"
 # Wait for success and touch passed file if it did
-if wait_for_startup testdb testtable value SUCCESS; then
-    echo "TEST PASSED (mariadb): Contents for testdb is correct"
-else
+if ! wait_for_startup testdb testtable value SUCCESS; then
+    fdc_test_fail mariadb "Detabase table value test failed"
     false
 fi
+fdc_test_pass mariadb "Database table value matches"
 
 
 # If we have GTID enabled, check its set to ON
 if [ -n "$MYSQL_CLUSTER_USE_GTID" ]; then
-    echo "TEST START (mariadb): Testing GTID is enabled"
+    fdc_test_start mariadb "Testing GTID is enabled"
     gtid_support=$(echo "SHOW GLOBAL VARIABLES WHERE Variable_name = 'wsrep_gtid_mode'" | mariadb -s 2>/dev/null | awk '{ print $2 }' || true )
     if [ "$gtid_support" != "ON" ]; then
-        echo "TEST FAILED (mariadb): GTID support does not seem to be enabled! result='$gtid_support'"
+        fdc_test_fail mariadb "GTID support does not seem to be enabled! result='$gtid_support'"
         exit 1
     fi
-    echo "TEST PASSED (mariadb): GTID is enabled!"
+    fdc_test_pass mariadb "GTID is enabled"
 fi
 
 
 # Next, on node2, create another database and table to test
-echo "TEST START (mariadb): Testing cluster-node2"
+fdc_test_start mairadb "Testing cluster-node2"
 if [ "$FDC_CI" = "cluster-node2" ]; then
-    echo "TEST PROGRESS (mariadb): Setting up cluster-node2 database 'testdb2' and table 'testtable'"
+    fdc_test_progress mariadb "Setting up cluster-node2 database 'testdb2' and table 'testtable'"
     echo "CREATE DATABASE testdb2" | mariadb -v
     echo "CREATE TABLE testtable (id INT AUTO_INCREMENT PRIMARY KEY, value TEXT);" | mariadb -v testdb2
     echo "INSERT INTO testtable (value) VALUES ('SUCCESS');" | mariadb -v testdb2
 fi
 # Wait for success and touch passed file if it did
-if wait_for_startup testdb2 testtable value SUCCESS; then
-    echo "TEST PASSED (mariadb): Database contents from cluster-node2 for testdb2 is correct"
-else
+if ! wait_for_startup testdb2 testtable value SUCCESS; then
+    fdc_test_fail mariadb "Failed database contents from cluster-node2 for 'testdb2'"
     false
 fi
-
+fdc_test_pass mariadb "Database contents from cluster-node2 for 'testdb2' is correct"
 
 
 # Next, on node3, create another database and table to test
-echo "TEST START (mariadb): Testing cluster-node3"
+fdc_test_start mariadb "Testing cluster-node3"
 if [ "$FDC_CI" = "cluster-node3" ]; then
-    echo "TEST PROGRESS (mariadb): Setting up cluster-node3 database 'testdb3' and table 'testtable'"
+    fdc_test_progress mariadb "Setting up cluster-node3 database 'testdb3' and table 'testtable'"
     echo "CREATE DATABASE testdb3" | mariadb -v
     echo "CREATE TABLE testtable (id INT AUTO_INCREMENT PRIMARY KEY, value TEXT);" | mariadb -v testdb3
     echo "INSERT INTO testtable (value) VALUES ('SUCCESS');" | mariadb -v testdb3
 fi
 # Wait for success and touch passed file if it did
-if wait_for_startup testdb3 testtable value SUCCESS; then
-    echo "TEST PASSED (mariadb): Database contents from cluster-node3 for testdb3 is correct"
-else
+if ! wait_for_startup testdb3 testtable value SUCCESS; then
+    fdc_test_fail mariadb "Failed atabase contents from cluster-node3 for table 'testdb3'"
     false
 fi
-
-
-
-echo "PASSED" > /PASSED_MARIADB
-
-echo "NOTICE: Done with MariaDB testing, waiting for shutdown"
-sleep 600
+fdc_test_pass mariadb "Database contents from cluster-node3 for 'testdb3' is correct"
