8426d0b4d6d38935c29dcced8454ce437e2811de
Nigel Kukard <nkukard@lbsd.net>
2023-06-10 03:29:51 +0000

    fix: use sed and not perl for regex replacements

--- usr/local/share/flexible-docker-containers/init.d/42-mariadb.sh
+++ usr/local/share/flexible-docker-containers/init.d/42-mariadb.sh
@@ -80,7 +80,7 @@ chown -R mysql:mysql /var/lib/mysql
 # Tuning
 if [ -n "$MYSQL_BUFFER_SIZE" ]; then
 	fdc_notice "Setting MariaDB 'innodb-buffer-pool-size' to $MYSQL_BUFFER_SIZE"
-	perl -pi -e "s/innodb-buffer-pool-size=.*/innodb-buffer-pool-size=$MYSQL_BUFFER_SIZE/" /etc/my.cnf.d/docker.cnf
+	sed -i -e "s/innodb-buffer-pool-size=.*/innodb-buffer-pool-size=$MYSQL_BUFFER_SIZE/" /etc/my.cnf.d/docker.cnf
 fi
 
 
